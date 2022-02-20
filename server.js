e981c75e343f2d6b49cbf540483c1c81630e6f1c
Nina2023 <nsehgal2019@gmail.com>
2022-02-20 12:45:16 -0500

    added error back

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // Require http module
-const http = require("http");
+const http = require("http")
 
 // Require fs module
-const fs = require("fs");
+const fs = require("fs")
 
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = require("minimist")(process.argv.slice(2));
+const args = require("minimist")(process.argv.slice(2))
 
 // Define allowed argument name 'port'.
-args["port"];
+args["port"]
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || process.env.PORT || 3000;
+const port = args.port || process.env.PORT || 3000
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
@@ -20,6 +20,12 @@ const port = args.port || process.env.PORT || 3000;
 fs.readFile("./www/index.html", "utf8", (err, data) => {
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
+
+if (err) {
+  console.error(err)
+  return
+}
+
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
